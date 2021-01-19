45769b753edca962c329b7046bf9f2aa340d1196
Fati <fati.pereira74@gmail.com>
2021-01-19 15:47:44 +0100

    Adding sidebar.php file and get_sidebar in index.php

---
+++ sidebar.php
@@ -0,0 +1,16 @@
+<div class="col-12 col-md-4 col-xl-3">
+        <div class="sidebar">
+            <aside>
+                <h2>Mon widget</h2>
+                <p>Contenu du widget</p>
+            </aside>
+            <aside>
+                <h2>Mon widget</h2>
+                <p>Contenu du widget</p>
+            </aside>
+            <aside>
+                <h2>Mon widget</h2>
+                <p>Contenu du widget</p>
+            </aside>
+        </div>
+    </div>
