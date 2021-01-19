df7a5a42b484e5eb4d9255e34d306e26c492c92a
Fati <fati.pereira74@gmail.com>
2021-01-19 16:00:04 +0100

    Adding post folder in template-parts folder

--- index.php
+++ index.php
@@ -5,23 +5,7 @@
     <div class="content col-12 col-md-8 col-xl-9">
         <h1>Le blog</h1>
         <div class="items row d-flex flex-wrap">
-            <div class="col-lg-6">
-                <article>
-                    <div class="infos">
-                        <div class="cats-date">
-                            <a href="">CAT1</a>,
-                            <a href="">CAT2</a>
-                            <time datetime="2018-06-20 16:20">/ 20 JUIN 2018</time>
-                        </div>
-                        <div class="tags d-flex flex-wrap">
-                            <a href="">tag1</a>
-                            <a href="">tag2</a>
-                        </div>
-                        <h1><a href="post.html">Le nom du post</a></h1>
-                    </div>
-                </article>
-            </div>
-            <div class="col-lg-6">
+            
         </div>
     </div>
 </div>
