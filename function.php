3dba82f70891a2930a7fd9f711ef791b7a71b700
Fati <fati.pereira74@gmail.com>
2021-01-19 10:14:16 +0100

    Initialization function.php file

---
+++ function.php
@@ -0,0 +1,10 @@
+<?php
+
+// Files CSS and JS
+
+function lya_add_theme_scripts() 
+{
+
+    // CSS 
+    wp_enqueue_style('bootstrap-grid', get_template_directory_uri() ./assets/css/bootstrap-grid.min.css)
+}
