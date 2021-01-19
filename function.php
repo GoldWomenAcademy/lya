581520bf4f5014493f058b9fe7477352c0369b67
Fati <fati.pereira74@gmail.com>
2021-01-19 10:31:29 +0100

    Adding style.css, bootstrap-grid in function.php file

--- function.php
+++ function.php
@@ -6,8 +6,10 @@ function lya_add_theme_scripts()
 {
     // CSS 
     wp_enqueue_style('bootstrap-grid', get_template_directory_uri() . '/assets/css/bootstrap-grid.min.css');
-
     wp_enqueue_style('fontawesome', 'https://use.fontawesome.com/releases/v5.15.1/css/all.css'); 
+    wp_enqueue_style('style', get_stylesheet_uri());
+
+    
 }
 
 add_action('wp_enqueue_scripts', 'lya_add_theme_scripts');
