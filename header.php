e68531feb30a3605850a162a557dcc03d281791c
Fati <fati.pereira74@gmail.com>
2021-01-19 15:36:29 +0100

    Adding header.php file

---
+++ header.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php wp_title(' - ', true, 'right'); ?><?php bloginfo('name'); ?></title>
+    <?php wp_head(); ?>
+</head>
+<body <?php body_class(); ?>>
+    <header>
+            <div class="container d-flex flex-wrap align-items-center">
+                <h1><a href="<?php bloginfo('url'); ?>"><?php bloginfo('name'); ?></a></h1>
+                <button id="burger" class="d-md-none">
+                    <i class="fas fa-bars"></i>
+                </button>
+                <nav class="menu d-md-block">
+                    <ul class="d-md-flex">
+                        <li><a href="index.html">Formations</a></li>
+                        <li><a href="blog.html">Blog</a></li>
+                        <li><a href="contact.html">Contact</a></li>
+                    </ul>
+                </nav>
+            </div>
+        </header>
+        <main class="container">   
