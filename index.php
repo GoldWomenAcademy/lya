6ab3d70756f49537f632844d0674a32352ab5fa2
Fati <fati.pereira74@gmail.com>
2021-01-19 16:16:15 +0100

    Adding get_template_part function in index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <div class="content col-12 col-md-8 col-xl-9">
         <h1>Le blog</h1>
         <div class="items row d-flex flex-wrap">
-            
+            <?php get_template_part('template-parts/post/content', 'post-and-formation'); ?>
         </div>
     </div>
 </div>
