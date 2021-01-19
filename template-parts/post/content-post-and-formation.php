d3b34b027ba4a40541e6cea5e889502925e0a471
Fati <fati.pereira74@gmail.com>
2021-01-19 18:14:30 +0100

    Update content-post-and-formation.php, index.php files

--- template-parts/post/content-post-and-formation.php
+++ template-parts/post/content-post-and-formation.php
@@ -1,16 +1,14 @@
-<div class="col-lg-6">
-                        <article>
-                            <div class="infos">
-                                <div class="cats-date">
-                                    <a href="">CAT1</a>,
-                                    <a href="">CAT2</a>
-                                    <time datetime="2018-06-20 16:20">/ 20 JUIN 2018</time>
-                                </div>
-                                <div class="tags d-flex flex-wrap">
-                                    <a href="">tag1</a>
-                                    <a href="">tag2</a>
-                                </div>
-                                <h1><a href="post.html">Le nom du post</a></h1>
-                            </div>
-                        </article>
-                    </div>
+<article>
+    <div class="infos">
+        <div class="cats-date">
+            <a href="">CAT1</a>,
+            <a href="">CAT2</a>
+            <time datetime="2018-06-20 16:20">/ 20 JUIN 2018</time>
+        </div>
+        <div class="tags d-flex flex-wrap">
+            <a href="">tag1</a>
+            <a href="">tag2</a>
+        </div>
+        <h1><a href="post.html">Le nom du post</a></h1>
+    </div>
+</article>
