8bbf44c1981a71b21de284479a26be79cdb840bc
Fati <fati.pereira74@gmail.com>
2021-01-19 17:56:08 +0100

    Adding get_template_part function in page-formations.php page

--- page-formations.php
+++ page-formations.php
@@ -1,39 +1,10 @@
 <?php get_header(); ?>
+
 <h1>Les formations</h1>
-        <div class="items row d-flex flex-wrap">
-            <div class="col-md-6 col-xl-4">
-                <article>
-                    <a href="formation.html"><img src="assets/img/103711.jpg" alt=""></a>
-                    <div class="infos">
-                        <div class="cats-date">
-                            <time datetime="2018-06-20 16:20">20 JUIN 2018</time>
-                        </div>
-                        <h1><a href="formation.html">Le nom de la formation</a></h1>
-                    </div>
-                </article>
-            </div>
-            <div class="col-md-6 col-xl-4">
-                <article>
-                    <a href="formation.html"><img src="assets/img/103711.jpg" alt=""></a>
-                    <div class="infos">
-                        <div class="cats-date">
-                            <time datetime="2018-06-20 16:20">20 JUIN 2018</time>
-                        </div>
-                        <h1><a href="formation.html">Le nom de la formation</a></h1>
-                    </div>
-                </article>
-            </div>
-            <div class="col-md-6 col-xl-4">
-                <article>
-                    <a href="formation.html"><img src="assets/img/103711.jpg" alt=""></a>
-                    <div class="infos">
-                        <div class="cats-date">
-                            <time datetime="2018-06-20 16:20">20 JUIN 2018</time>
-                        </div>
-                        <h1><a href="formation.html">Le nom de la formation</a></h1>
-                    </div>
-                </article>
-            </div>
+    <div class="items row d-flex flex-wrap">
+        <div class="col-md-6 col-xl-4">
+        <?php get_template_part('template-parts/post/content', 'post-and-formation'); ?>
         </div>
+    </div>
 
 <?php get_footer(); ?>
