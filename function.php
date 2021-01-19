d2a5b9e16efd9a45d426ea70e389473f7811bb38
Fati <fati.pereira74@gmail.com>
2021-01-19 10:26:42 +0100

    Adding fontawesome url in function.php file

--- function.php
+++ function.php
@@ -4,7 +4,10 @@
 
 function lya_add_theme_scripts() 
 {
-
     // CSS 
-    wp_enqueue_style('bootstrap-grid', get_template_directory_uri() ./assets/css/bootstrap-grid.min.css)
+    wp_enqueue_style('bootstrap-grid', get_template_directory_uri() . '/assets/css/bootstrap-grid.min.css');
+
+    wp_enqueue_style('fontawesome', 'https://use.fontawesome.com/releases/v5.15.1/css/all.css'); 
 }
+
+add_action('wp_enqueue_scripts', 'lya_add_theme_scripts');
