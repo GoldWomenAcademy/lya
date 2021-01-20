7f6d18362e852895916e69dfb10620f374effdd8
Fati <fati.pereira74@gmail.com>
2021-01-20 11:47:35 +0100

    Adding the_title function in content-post-and-formation.php file

--- template-parts/post/content-post-and-formation.php
+++ template-parts/post/content-post-and-formation.php
@@ -4,7 +4,7 @@
         <?php the_category(', '); ?>
             <time datetime="<?php the_time('Y-m-d H:i') ?>">/ <?php the_time('j F Y') ?></time>
         </div>
-        <?php the_tags('<div class="tags d-flex flex-wrap">', '', '</div>') ?>
-        <h1><a href="post.html">Le nom du post</a></h1>
+        <?php the_tags('<div class="tags d-flex flex-wrap">', '', '</div>'); ?>
+        <h1><a href="post.html"><?php the_title(); ?></a></h1>
     </div>
 </article>
