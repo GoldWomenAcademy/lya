d28c2a659fe55cbbecc34796e33de4bc79538f84
Fati <fati.pereira74@gmail.com>
2021-01-20 12:06:36 +0100

    Update content-post-and-formation.php file

--- single.php
+++ single.php
@@ -1,4 +1,34 @@
 <?php get_header(); ?>
 
+<div class="items item">
+            <article>
+                <div class="nextandprev d-flex flex-wrap justify-content-between">
+                    <div class="next">
+                        <a href="">LIEN VERS LE POST PLUS RECENT</a>
+                    </div>
+                    <div class="prev">
+                        <a href="">LIEN VERS LE POST PLUS ANCIEN</a>
+                    </div>
+                </div>
+                <div class="infos">
+                    <div class="cats-date">
+                        <a href="">CAT1</a>,
+                        <a href="">CAT2</a>
+                        <time datetime="2018-06-20 16:20">/ 20 JUIN 2018</time>
+                    </div>
+                    <div class="tags d-flex flex-wrap">
+                        <a href="">tag1</a>
+                        <a href="">tag2</a>
+                    </div>
+                    <h1>Le nom du post</h1>
+                    <div class="content">
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odit deserunt blanditiis, dolor, rerum totam optio, odio repellat tempora est reiciendis debitis nisi enim. Quasi dolorum culpa itaque repellendus mollitia tempore cumque pariatur, delectus error dolor quia ea neque veritatis blanditiis porro totam, accusamus ducimus nobis ipsum! Perferendis aut omnis rem ea voluptatum vero, dolores, aliquid amet recusandae reprehenderit assumenda distinctio eveniet voluptate consequuntur blanditiis sequi</p>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odit deserunt blanditiis, dolor, rerum totam optio, odio repellat tempora est reiciendis debitis nisi enim. Quasi dolorum culpa itaque repellendus mollitia tempore cumque pariatur, delectus error dolor quia ea neque veritatis blanditiis porro totam, accusamus ducimus nobis ipsum! Perferendis aut omnis rem ea voluptatum vero, dolores, aliquid amet recusandae reprehenderit assumenda distinctio eveniet voluptate consequuntur blanditiis sequi</p>
+                    </div>
+                </div>
+            </article>
+            <!-- Les commentaires -->
+        </div>
+
 
 <?php get_footer(); ?>
--- template-parts/post/content-post-and-formation.php
+++ template-parts/post/content-post-and-formation.php
@@ -1,8 +1,18 @@
 <article>
+    <?php if (get_post_type() === 'formation') : ?>
+        <a href="<?php the_permalink(); ?>"><?php the_post_thumbnail(); ?></a>
+    <?php endif; ?>
     <div class="infos">
         <div class="cats-date">
-        <?php the_category(', '); ?>
-            <time datetime="<?php the_time('Y-m-d H:i'); ?>">/ <?php the_time('j F Y') ?></time>
+            <?php the_category(', '); ?>
+            <time datetime="<?php the_time("Y-m-d H:i"); ?>">
+                <?php
+                if (get_post_type() != 'formation') {
+                    echo '/';
+                }
+                ?>
+                <?php the_time("j F Y"); ?>
+            </time>
         </div>
         <?php the_tags('<div class="tags d-flex flex-wrap">', '', '</div>'); ?>
         <h1><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
