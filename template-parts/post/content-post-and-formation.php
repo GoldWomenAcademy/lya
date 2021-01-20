0e09ece4d6ec2ed96c531f5d7586b22675c1daf7
Fati <fati.pereira74@gmail.com>
2021-01-20 11:34:42 +0100

    Adding the_tags function in content-post-and-formation.php

--- template-parts/post/content-post-and-formation.php
+++ template-parts/post/content-post-and-formation.php
@@ -2,12 +2,9 @@
     <div class="infos">
         <div class="cats-date">
         <?php the_category(', '); ?>
-            <time datetime="2018-06-20 16:20">/ <?php the_time('j F Y') ?></time>
+            <time datetime="<?php the_time('Y-m-d H:i') ?>">/ <?php the_time('j F Y') ?></time>
         </div>
-        <div class="tags d-flex flex-wrap">
-            <a href="">tag1</a>
-            <a href="">tag2</a>
-        </div>
+        <?php the_tags('<div class="tags d-flex flex-wrap">', '', '</div>') ?>
         <h1><a href="post.html">Le nom du post</a></h1>
     </div>
 </article>
