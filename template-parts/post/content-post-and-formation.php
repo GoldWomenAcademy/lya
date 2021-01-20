7b94204351650c6035a74b606e80d738b33c6206
Fati <fati.pereira74@gmail.com>
2021-01-20 11:51:04 +0100

    Adding the_permalink function in content-post-and-formation.php file

--- template-parts/post/content-post-and-formation.php
+++ template-parts/post/content-post-and-formation.php
@@ -2,9 +2,9 @@
     <div class="infos">
         <div class="cats-date">
         <?php the_category(', '); ?>
-            <time datetime="<?php the_time('Y-m-d H:i') ?>">/ <?php the_time('j F Y') ?></time>
+            <time datetime="<?php the_time('Y-m-d H:i'); ?>">/ <?php the_time('j F Y') ?></time>
         </div>
         <?php the_tags('<div class="tags d-flex flex-wrap">', '', '</div>'); ?>
-        <h1><a href="post.html"><?php the_title(); ?></a></h1>
+        <h1><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
     </div>
 </article>
