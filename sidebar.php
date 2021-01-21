00c3422c7550458063f6180def71da5483e9b544
Fati <fati.pereira74@gmail.com>
2021-01-21 13:31:22 +0100

    Update sidebar.php file

--- sidebar.php
+++ sidebar.php
@@ -1,5 +1,5 @@
 <div class="col-12 col-md-4 col-xl-3">
         <div class="sidebar">
-           <?php dynamic_sidebar('blog') ?>
+           <?php dynamic_sidebar('blog'); ?>
         </div>
     </div>
